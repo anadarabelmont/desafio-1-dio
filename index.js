84416bb09a4d30b2147df194b506136c428914b7
Anadara Belmont <anadarabelmont01@gmail.com>
2023-12-01 16:43:06 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("olá mundoooo");
+let nome = "Anadara";
+let xp = 8000;
+let nivel = "";
+
+if (xp <= 1000) {
+  nivel = "ferro";
+} else if (xp <= 2001) {
+  nivel = "bronze";
+} else if (xp <= 5000) {
+  nivel = "prata";
+} else if (xp <= 7000) {
+  nivel = "ouro";
+} else if (xp <= 8000) {
+  nivel = "platina";
+} else if (xp <= 9000) {
+  nivel = "ascendente";
+} else if (xp <= 10000) {
+  nivel = "imortal";
+} else if (xp >= 10000) {
+  nivel = "radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
